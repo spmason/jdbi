8f87d9a5407e83fce0882af2c8540259b66ed679
Marnes <marnick.leau@gmail.com>
2019-07-03 21:02:29 +0200

    clean declaration

--- sqlobject/src/test/java/org/jdbi/v3/sqlobject/locator/TestSqlLocator.java
+++ sqlobject/src/test/java/org/jdbi/v3/sqlobject/locator/TestSqlLocator.java
@@ -55,7 +55,7 @@ public class TestSqlLocator {
     }
 
     public static class TestConfig implements JdbiConfig<TestConfig> {
-        String sql;
+        private String sql;
 
         public TestConfig() {}
 
